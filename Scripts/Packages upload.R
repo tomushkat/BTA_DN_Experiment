76670663dc2b96567607f3fbd096044a0bf85b6c
tomushkat <tmushkat@gmail.com>
2021-12-14 12:18:32 +0200

    first libraries

---
+++ Scripts/Packages upload.R
@@ -0,0 +1,1 @@
+pacman::p_load(doSPSS, readxl, tidyverse, here, skimr, anytime, car, effectsize, gmodels, apaTables, psych, rcompanion, ResourceSelection, lmtest, faraway, estimatr, tidymodels, hdm)
